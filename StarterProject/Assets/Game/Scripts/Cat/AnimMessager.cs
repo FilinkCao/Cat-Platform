6dc930fd08e954ed5c5f4c160bc6a92698b7f518
Adsew <30912391+Adsew@users.noreply.github.com>
2018-03-06 13:19:07 -0500

    Animator for cat set up (needs animations) Anim controller script to pass messages to the animator complete.

---
+++ StarterProject/Assets/Game/Scripts/Cat/AnimMessager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimMessager : MonoBehaviour {
+
+    private Animator anim = null;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+	}
+	
+    public void sendTriggerMessage(string varName) {
+
+        if (anim != null) {
+
+            anim.SetTrigger(varName);
+        }
+    }
+
+    public void sendFloatMessage(string varName, float val) {
+
+        if (anim != null) {
+
+            anim.SetFloat(varName, val);
+        }
+    }
+
+    public void sendIntMessage(string varName, int val) {
+
+        if (anim != null) {
+
+            anim.SetInteger(varName, val);
+        }
+    }
+
+    public void sendBoolMessage(string varName, bool val) {
+
+        if (anim != null) {
+
+            anim.SetBool(varName, val);
+        }
+    }
+}
