a38c2e90f26055c12f56fb4b15380959daef65f7
Adsew <30912391+Adsew@users.noreply.github.com>
2018-03-07 16:17:43 -0500

    Order layers fixed, testing reenabled

--- StarterProject/Assets/Game/Scripts/Cat/FSMBehaviours/CatJumpingBehaviour.cs
+++ StarterProject/Assets/Game/Scripts/Cat/FSMBehaviours/CatJumpingBehaviour.cs
@@ -38,7 +38,7 @@ public class CatJumpingBehaviour : StateMachineBehaviour {
 
         foreach (GameObject p in CatController.Instance.platforms)
         {
-            if (p != null && p != CatController.Instance.standOn)
+            if (p != null && p != CatController.Instance.standOn && p.gameObject.tag != "Ground")
             {
                 Vector2 toP = p.transform.position - CatController.Instance.gameObject.transform.position;
                 float angle = Vector2.Angle(toFish.normalized, toP.normalized);
@@ -59,17 +59,17 @@ public class CatJumpingBehaviour : StateMachineBehaviour {
     {
         if (target != null)
         {
-            float x = Mathf.Clamp(CatController.Instance.gameObject.transform.position.x, target.transform.position.x - platform.GetComponent<SpriteRenderer>().size.x / 2f, target.transform.position.x + platform.GetComponent<SpriteRenderer>().size.x / 2f) - CatController.Instance.gameObject.transform.position.x;
-            float y = target.transform.position.y - CatController.Instance.gameObject.transform.position.y;
+            //float x = Mathf.Clamp(CatController.Instance.gameObject.transform.position.x, target.transform.position.x - platform.GetComponent<SpriteRenderer>().size.x / 2f, target.transform.position.x + platform.GetComponent<SpriteRenderer>().size.x / 2f) - CatController.Instance.gameObject.transform.position.x;
+            //float y = target.transform.position.y - CatController.Instance.gameObject.transform.position.y;
 
-            //float jumpAngle = Mathf.Atan(((jumpVelo * jumpVelo + Mathf.Sqrt(Mathf.Pow(jumpVelo, 4) - Physics2D.gravity.y * (Physics2D.gravity.y * x * Physics2D.gravity.y * x + 2 * y * jumpVelo * jumpVelo))) / (Physics2D.gravity.y * x)));
+            ////float jumpAngle = Mathf.Atan(((jumpVelo * jumpVelo + Mathf.Sqrt(Mathf.Pow(jumpVelo, 4) - Physics2D.gravity.y * (Physics2D.gravity.y * x * Physics2D.gravity.y * x + 2 * y * jumpVelo * jumpVelo))) / (Physics2D.gravity.y * x)));
             
-            float verticalIniVelo = Mathf.Sqrt(2 * -Physics2D.gravity.y * (y + 1f));
-            float flightTime = verticalIniVelo / (-Physics2D.gravity.y);
-            float horizontalIniVelo = x / flightTime;
+            //float verticalIniVelo = Mathf.Sqrt(2 * -Physics2D.gravity.y * (y + 1f));
+            //float flightTime = verticalIniVelo / (-Physics2D.gravity.y);
+            //float horizontalIniVelo = x / flightTime;
 
 
-            /*
+            
             float x = target.transform.position.x - CatController.Instance.gameObject.transform.position.x;
             float y = target.transform.position.y - CatController.Instance.gameObject.transform.position.y;
 
@@ -86,6 +86,8 @@ public class CatJumpingBehaviour : StateMachineBehaviour {
                 (Mathf.Pow(velocity, 2) + rootedVal) / gravity * x
             );
 
+            angle = Mathf.Abs(angle);
+
             Debug.Log(angle);
 
             Vector2 trajectory = new Vector2();
@@ -95,15 +97,17 @@ public class CatJumpingBehaviour : StateMachineBehaviour {
             CatController.Instance.transform.Rotate(Vector3.forward, angle);
 
             CatController.Instance.gameObject.GetComponent<Rigidbody2D>().velocity = CatController.Instance.transform.right * velocity;
-            */
 
-            CatController.Instance.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalIniVelo, verticalIniVelo);
+
+            CatController.Instance.transform.right = Vector3.right;
+
+            //CatController.Instance.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalIniVelo, verticalIniVelo);
 
             CatController.Instance.gameObject.GetComponent<Animator>().SetTrigger("falling");
         }
         else
         {
-            CatController.Instance.gameObject.GetComponent<Animator>().SetTrigger("land");
+            CatController.Instance.gameObject.GetComponent<Animator>().SetTrigger("walk");
         }
     }
 }
