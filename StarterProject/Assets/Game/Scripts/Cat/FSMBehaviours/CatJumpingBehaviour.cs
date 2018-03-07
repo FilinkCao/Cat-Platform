943954ed9794f16a4f26e65f7f0d08d092c387e7
Adsew <30912391+Adsew@users.noreply.github.com>
2018-03-07 17:07:46 -0500

    Jumping working (mostly).

--- StarterProject/Assets/Game/Scripts/Cat/FSMBehaviours/CatJumpingBehaviour.cs
+++ StarterProject/Assets/Game/Scripts/Cat/FSMBehaviours/CatJumpingBehaviour.cs
@@ -4,29 +4,25 @@ using UnityEngine;
 
 public class CatJumpingBehaviour : StateMachineBehaviour {
 
-    private GameObject platform;
-
-    public float velocity = 2.0f;
+    public float velocityMultiplier = 8.0f;
 
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        platform = CatController.Instance.standOn;
-
         ChooseAPlatformToJump();
     }
 
-    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+    ////OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
 
-    }
+    //}
 
-    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+    ////OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
 
-    }
+    //}
 
     // Determine which platform to jump to
     void ChooseAPlatformToJump()
@@ -59,20 +55,10 @@ public class CatJumpingBehaviour : StateMachineBehaviour {
     {
         if (target != null)
         {
-            //float x = Mathf.Clamp(CatController.Instance.gameObject.transform.position.x, target.transform.position.x - platform.GetComponent<SpriteRenderer>().size.x / 2f, target.transform.position.x + platform.GetComponent<SpriteRenderer>().size.x / 2f) - CatController.Instance.gameObject.transform.position.x;
-            //float y = target.transform.position.y - CatController.Instance.gameObject.transform.position.y;
-
-            ////float jumpAngle = Mathf.Atan(((jumpVelo * jumpVelo + Mathf.Sqrt(Mathf.Pow(jumpVelo, 4) - Physics2D.gravity.y * (Physics2D.gravity.y * x * Physics2D.gravity.y * x + 2 * y * jumpVelo * jumpVelo))) / (Physics2D.gravity.y * x)));
-            
-            //float verticalIniVelo = Mathf.Sqrt(2 * -Physics2D.gravity.y * (y + 1f));
-            //float flightTime = verticalIniVelo / (-Physics2D.gravity.y);
-            //float horizontalIniVelo = x / flightTime;
-
-
-            
             float x = target.transform.position.x - CatController.Instance.gameObject.transform.position.x;
-            float y = target.transform.position.y - CatController.Instance.gameObject.transform.position.y;
+            float y = target.transform.position.y - CatController.Instance.gameObject.transform.position.y + 1.0f;
 
+            float velocity = Vector2.SqrMagnitude(new Vector2(x, y));
             float gravity = Physics2D.gravity.y;
 
             float rootedVal = Mathf.Sqrt(
@@ -82,27 +68,34 @@ public class CatJumpingBehaviour : StateMachineBehaviour {
                         + 2.0f * y * Mathf.Pow(velocity, 2))
             );
 
-            float angle = Mathf.Rad2Deg * Mathf.Atan(
-                (Mathf.Pow(velocity, 2) + rootedVal) / gravity * x
-            );
+            float angle = 0;
 
-            angle = Mathf.Abs(angle);
-
-            Debug.Log(angle);
-
-            Vector2 trajectory = new Vector2();
-            trajectory.x = CatController.Instance.transform.right.x;
-            trajectory.y = CatController.Instance.transform.right.y;
-
-            CatController.Instance.transform.Rotate(Vector3.forward, angle);
-
-            CatController.Instance.gameObject.GetComponent<Rigidbody2D>().velocity = CatController.Instance.transform.right * velocity;
-
-
-            CatController.Instance.transform.right = Vector3.right;
-
-            //CatController.Instance.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalIniVelo, verticalIniVelo);
-
+            if (y >= 0)
+            {
+                angle = Mathf.Rad2Deg * Mathf.Atan((Mathf.Pow(velocity, 2) - rootedVal) / gravity * x);
+            }
+            else
+            {
+                angle = Mathf.Rad2Deg * Mathf.Atan((Mathf.Pow(velocity, 2) + rootedVal) / gravity * x);
+            }
+            
+            Debug.Log("Angle " + angle);
+            Debug.Log("Velocity " + velocity);
+            
+            if (angle >= 0)
+            {
+                CatController.Instance.transform.Rotate(Vector3.forward, angle);
+                CatController.Instance.gameObject.GetComponent<Rigidbody2D>().velocity = CatController.Instance.transform.right * velocity;
+                CatController.Instance.transform.right = Vector3.right;
+            }
+            else
+            {
+                CatController.Instance.transform.Rotate(Vector3.up, 180.0f);
+                CatController.Instance.transform.Rotate(Vector3.forward, Mathf.Abs(angle));
+                CatController.Instance.gameObject.GetComponent<Rigidbody2D>().velocity = CatController.Instance.transform.right * velocity;
+                CatController.Instance.transform.right = Vector3.left;
+            }
+            
             CatController.Instance.gameObject.GetComponent<Animator>().SetTrigger("falling");
         }
         else
