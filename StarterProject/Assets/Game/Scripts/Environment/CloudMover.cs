0d438fa0763a696c042031562217d9a7a5b61d4e
Adsew <30912391+Adsew@users.noreply.github.com>
2018-03-07 20:03:58 -0500

    Clouds added. Randomly spawn and move across screen then die.

---
+++ StarterProject/Assets/Game/Scripts/Environment/CloudMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMover : MonoBehaviour {
+
+    public float speed = -0.3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(new Vector3(speed, 0.0f, 0.0f));
+	}
+}
---
+++ StarterProject/Assets/Game/Scripts/Environment/CloudSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour {
+
+    private float timer = 0;
+    private float timeBetweenClouds = 3.0f;
+
+    public List<GameObject> objectsToSpawn = new List<GameObject>();
+    public List<GameObject> spawnLocations = new List<GameObject>();
+
+    public float timeBetweenCloudsMin = 3.0f;
+    public float timeBetweenCloudsMax = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenClouds)
+        {
+            timer = 0.0f;
+            timeBetweenClouds = Random.Range(timeBetweenCloudsMin, timeBetweenCloudsMax);
+
+            if (spawnLocations.Count > 0 && objectsToSpawn.Count > 0)
+            {
+                GameObject spawnedObj = Instantiate(objectsToSpawn[Random.Range(0, objectsToSpawn.Count)]);
+
+                if (spawnedObj != null)
+                {
+                    spawnedObj.transform.position = spawnLocations[Random.Range(0, spawnLocations.Count)].transform.position;
+                }
+            }
+        }
+	}
+}
